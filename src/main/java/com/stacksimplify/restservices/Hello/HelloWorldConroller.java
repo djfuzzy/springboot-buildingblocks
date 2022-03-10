25ba707435417a4d1ef200e703017c620d576855
Dillon Forsberg <forsbed1@fcc.ca>
2022-03-10 15:16:40 -0600

    Hello World Base

---
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldConroller.java
@@ -0,0 +1,21 @@
+package com.stacksimplify.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldConroller {
+
+	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+	@GetMapping("/helloworld1")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/helloworldbean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Dillon", "James", "Forsberg");
+	}
+}
